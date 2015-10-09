f62c714705ddc0a7a0de6a6e485abb17902c2a4c
Roy Gonzalez <roy.gonzalez@aurea.com>
2015-10-09 04:40:20 -0600

    Adding Config

---
+++ Src/MCode.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿// ***********************************************************************
+// Assembly         : MCode.Common
+// Author           : Roy
+// Created          : 10-06-2015
+//
+// Last Modified By : Roy
+// Last Modified On : 10-06-2015
+// ***********************************************************************
+// <copyright file="AssemblyInfo.cs" company="MCode">
+//     Copyright ©  2015 MCode
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MCode.Common")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MCode")]
+[assembly: AssemblyProduct("MCode.Common")]
+[assembly: AssemblyCopyright("Copyright ©  2015 MCode")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("2c962f08-f2f0-421e-8f98-97aeaf8a8514")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Src/MCode.Core/Configuration/Config.cs
@@ -0,0 +1,29 @@
+﻿// ***********************************************************************
+// Assembly         : MCode.Core
+// Author           : Roy
+// Created          : 10-06-2015
+//
+// Last Modified By : Roy
+// Last Modified On : 10-06-2015
+// ***********************************************************************
+// <copyright file="Config.cs" company="MCore">
+//     © 2015 MCore
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace MCode.Core.Configuration
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Class Config.
+    /// </summary>
+    public class Config : IConfig
+    {
+        /// <summary>
+        /// Gets or sets the settings.
+        /// </summary>
+        /// <value>The settings.</value>
+        public IDictionary<string, string> Settings { get; set; }
+    }
+}
