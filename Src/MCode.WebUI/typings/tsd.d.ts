6de3db5a299f871bedb9d76e9e479117bea82d47
roy.gonzalez <roy.gonzalez@aurea.com>
2016-01-31 22:07:29 -0600

    Removing WebUIOld

--- Src/MCode.WebUI/typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="angularjs/angular.d.ts" />
-/// <reference path="jsdom/jsdom.d.ts" />
-/// <reference path="requirejs/require.d.ts" />
-/// <reference path="react/react.d.ts" />
